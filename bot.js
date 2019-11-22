de8fd1b40083a15c5840d4ea1914b432fb29c97a
lodydoma <58074075+lodydoma@users.noreply.github.com>
2019-11-22 14:56:09 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("647398030479458307")
+setInterval(function() {
+channel.send(`lodydoma is the best`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
